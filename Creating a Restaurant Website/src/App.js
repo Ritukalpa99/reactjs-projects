99c73da97a2c5f844b12e66253ca53b52db19c08
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-05-18 15:46:55 +0530

    Add summary done

--- Creating a Restaurant Website/src/App.js
+++ Creating a Restaurant Website/src/App.js
@@ -1,10 +1,10 @@
 import Header from "./components/Layout/Header";
-
+import Meals from "./components/Meals/Meals";
 function App() {
   return (
     <>
-      <h2>Let's get started!</h2>
       <Header />
+      <Meals />
     </>
   );
 }
---
+++ Creating a Restaurant Website/src/components/Meals/Meals.js
@@ -0,0 +1,11 @@
+import classes from "./Meals.module.css"
+
+const Meals = () => {
+    return <section className={classes.section}>
+    <h4>Delicious Food, Delivered To you</h4>
+    <p>Choose your favorite meal from our broad selection of available means and enjoy a delicious lunch or dinner at home</p>
+    <p>All our meals are cooked with high-quality ingredients, just-in time and of course by experienced chefs!</p>
+    </section>
+}
+
+export default Meals;
