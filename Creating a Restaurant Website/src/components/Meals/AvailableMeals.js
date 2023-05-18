d8adb6f4bf4611b79627180ffe720bea321ba7c7
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-05-18 17:50:12 +0530

    Item visible refactor

--- Creating a Restaurant Website/src/components/Meals/AvailableMeals.js
+++ Creating a Restaurant Website/src/components/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
 import classes from './AvailableMeals.module.css';
-
+import Card from '../UI/Card';
+import MealItem from "./MealItem/MealItem"
 const DUMMY_MEALS = [
     {
       id: 'm1',
@@ -28,16 +29,14 @@ const DUMMY_MEALS = [
   ];
 const AvailableMeals = () => {
     const mealsList = DUMMY_MEALS.map(meal =>
-    <li>
-        <p>{meal.name}</p>
-        <p>{meal.description}</p>
-        <p className={classes.price}>${meal.price}</p>
-    </li>   
+    <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}/> 
     );
    return <section className={classes.meals}>
-    <ul>
-        {mealsList}  
-    </ul>
+    <Card>
+        <ul>
+            {mealsList}  
+        </ul>
+    </Card>
    </section>
 }
 
