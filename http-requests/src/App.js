b2ab0f30131882c57b549734cc339f14d10bb5a1
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-05-30 15:55:20 +0530

    load the app task done

--- http-requests/src/App.js
+++ http-requests/src/App.js
@@ -5,26 +5,29 @@ import "./App.css";
 
 function App() {
 	const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-	function fetchMoviesHandler() {
-		fetch("https://swapi.dev/api/films")
-			.then((res) => {
-				return res.json();
-			})
-			.then((data) => {
-				const transformedMovies = data.results.map((movieData) => {
-					return {
-						id: movieData.episode_id,
-						title: movieData.title,
-						openingText: movieData.opening_crawl,
-						releaseDate: movieData.release_date,
-					};
-				});
-				setMovies(transformedMovies);
-			});
-	}
+
+	// function fetchMoviesHandler() {
+	// 	fetch("https://swapi.dev/api/films")
+	// 		.then((res) => {
+	// 			return res.json();
+	// 		})
+	// 		.then((data) => {
+	// 			const transformedMovies = data.results.map((movieData) => {
+	// 				return {
+	// 					id: movieData.episode_id,
+	// 					title: movieData.title,
+	// 					openingText: movieData.opening_crawl,
+	// 					releaseDate: movieData.release_date,
+	// 				};
+	// 			});
+	// 			setMovies(transformedMovies);
+	// 		});
+	// }
 
 	async function fetchMoviesHandlerAsync() {
+    setIsLoading(true);
 		const res = await fetch("https://swapi.dev/api/films");
 		const data = await res.json();
 		const transformedMovies = data.results.map((movieData) => {
@@ -36,6 +39,7 @@ function App() {
 			};
 		});
 		setMovies(transformedMovies);
+    setIsLoading(false);
 	}
 
 	return (
@@ -44,7 +48,8 @@ function App() {
 				<button onClick={fetchMoviesHandlerAsync}>Fetch Movies</button>
 			</section>
 			<section>
-				<MoviesList movies={movies} />
+				{!isLoading && <MoviesList movies={movies} /> }
+        {isLoading && <p>Loading ...</p>}
 			</section>
 		</React.Fragment>
 	);
