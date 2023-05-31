876fd3b9c46e6604bd2addda387bfa74ae7dc5cf
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-05-31 18:09:26 +0530

    auth task done

---
+++ Authentication/src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+// import { Fragment } from 'react';
+import React from 'react';
+
+import MainNavigation from './MainNavigation';
+
+const Layout = (props) => {
+  return (
+    <React.Fragment>
+      <MainNavigation />
+      <main>{props.children}</main>
+    </React.Fragment>
+  );
+};
+
+export default Layout;
