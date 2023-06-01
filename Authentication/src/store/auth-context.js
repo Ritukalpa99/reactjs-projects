6c5769ff291bc8f399b382a28fc2ff83f7528074
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-06-01 10:47:53 +0530

    logout issues task done

--- Authentication/src/store/auth-context.js
+++ Authentication/src/store/auth-context.js
@@ -1,35 +1,39 @@
-import React, {useState} from "react"
+import React, { useState } from "react";
 const AuthContext = React.createContext({
-    token : '',
-    isLoggedIn : false,
-    login: (token) => {},
-    logout: () =>{}
-})
+	token: "",
+	isLoggedIn: false,
+	login: (token) => {},
+	logout: () => {},
+});
 
 export const AuthContextProvdier = (props) => {
-    const [token,setToken] = useState(null)
+	const initalToken = localStorage.getItem("token");
+	const [token, setToken] = useState(initalToken);
 
-    const userIsLoggedIn = !!token;
+	const userIsLoggedIn = !!token;
 
-    const loginHandler = (token) => {
-        setToken(token)
-    }
+	const loginHandler = (token) => {
+		setToken(token);
+		localStorage.setItem("token", token);
+	};
 
-    const logoutHandler = () => {
-        setToken(null)
-    }
+	const logoutHandler = () => {
+		setToken(null);
+		localStorage.removeItem("token");
+	};
 
-    const contextValue = {
-        token : token,
-        isLoggedIn : userIsLoggedIn,
-        login : loginHandler,
-        logout : logoutHandler
-    }
+	const contextValue = {
+		token: token,
+		isLoggedIn: userIsLoggedIn,
+		login: loginHandler,
+		logout: logoutHandler,
+	};
 
-    return <AuthContext.Provider value={contextValue}>
-        {props.children}
-    </AuthContext.Provider>
+	return (
+		<AuthContext.Provider value={contextValue}>
+			{props.children}
+		</AuthContext.Provider>
+	);
 };
 
-
-export default AuthContext
+export default AuthContext;
