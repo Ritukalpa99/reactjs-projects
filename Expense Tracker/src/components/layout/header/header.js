dfcb77d31a0565b12e90ce45c1a5028ba6efcecd
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-06-07 14:39:57 +0530

    logout user task done

--- Expense Tracker/src/components/layout/header/header.js
+++ Expense Tracker/src/components/layout/header/header.js
@@ -1,8 +1,18 @@
 import classes from "./header.module.css";
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import AuthContext from "../../../store/auth-context";
+import { useContext } from "react";
 const Header = () => {
+    const authCtx = useContext(AuthContext);
+    const navigate = useNavigate();
+    const logHandler = () => {
+        authCtx.logout();
+        navigate('/')
+    }
     return <nav className={classes.navbar}>
         <h1>Expense Tracker</h1>
+        {authCtx.isLoggedIn &&
+        <h3 className={classes.log} onClick={logHandler}>Logout</h3>}
         <div className={classes['profile-msg']}>
             Your profile is incomplete
             <Link to="/update-user">Complete Now</Link>
