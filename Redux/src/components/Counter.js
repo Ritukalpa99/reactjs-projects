8ee8b4cc1839e38ca1b55d3140689e7b3469aa3a
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-06-10 14:02:56 +0530

    migrating to redux-toolkit

--- Redux/src/components/Counter.js
+++ Redux/src/components/Counter.js
@@ -1,33 +1,38 @@
-import classes from './Counter.module.css';
-import { useSelector, useDispatch } from 'react-redux';
-
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "../store/index";
 const Counter = () => {
+	const dispatch = useDispatch();
 
-  const dispatch = useDispatch();
+	const counter = useSelector((state) => state.counter);
+	const show = useSelector((state) => state.showCounter);
+	const incrementHandler = () => {
+		dispatch(counterActions.increment());
+	};
 
-  const counter = useSelector(state => state.counter)
+	const increaseHandler = () => {
+		dispatch(counterActions.increase(5)); // {type : SOME_UNQUE_ID, payload : 5}
+	};
+	const decrementHandler = () => {
+		dispatch(counterActions.decrement());
+	};
 
-  const incrementHandler = () =>{
-    dispatch({type : 'increment'})
-  }
+	const toggleCounterHandler = () => {
+		dispatch({ type: "toggle" });
+	};
 
-  const decrementHandler = () =>{
-    dispatch({type : 'decrement'})
-  }
-
-  const toggleCounterHandler = () => {};
-
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
-      <div>
-        <button onClick={incrementHandler}>Increment</button>
-        <button onClick={decrementHandler}>Decrement</button>
-      </div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
+	return (
+		<main className={classes.counter}>
+			<h1>Redux Counter</h1>
+			{show && <div className={classes.value}>{counter}</div>}
+			<div>
+				<button onClick={incrementHandler}>Increment</button>
+				<button onClick={increaseHandler}>Increase by 5</button>
+				<button onClick={decrementHandler}>Decrement</button>
+			</div>
+			<button onClick={toggleCounterHandler}>Toggle Counter</button>
+		</main>
+	);
 };
 
 export default Counter;
